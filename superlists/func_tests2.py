7f92f748e03379885079dbac81a1493fa117539c
JUnho <554802601@qq.com>
2016-10-16 23:42:36 +0800

    20161026 Edit

--- superlists/func_tests2.py
+++ superlists/func_tests2.py
@@ -11,7 +11,7 @@ class NewVisionTest(unittest.TestCase):
 
     def test_cat_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://127.0.0.1:8000')
-        self.assertIn('To_Do',self.browser.title)
+        self.assertIn('To-Do',self.browser.title)
         self.fail('Finish the Test!')
 
 if __name__=='__main__':
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,6 +1,17 @@
 from django.test import TestCase
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 
-# Create your tests here.
+from lists.views import home_page
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve("/")
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_return_corrent_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
+
